3dcc4f6fcec90bacc19d739679d7fced6bf631f6
opmvpc <thibsix@outlook.be>
2020-12-02 20:43:51 +0100

    solution exo 1

--- src/Day1/ReportRepair.php
+++ src/Day1/ReportRepair.php
@@ -1,9 +1,35 @@
 <?php
 
-namespace Opmvpc\Advent\Day1;
-
 declare(strict_types=1);
+
+namespace Opmvpc\Advent\Day1;
 
 class ReportRepair
 {
+    public static function findNumbersWhereSumEquals2020(array $data): array
+    {
+        $result = [];
+        foreach ($data as $number) {
+            foreach ($data as $otherNumber) {
+                if ($number + $otherNumber === 2020) {
+                    $result[0] = $number;
+                    $result[1] = $otherNumber;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public static function multiplication(array $data): int
+    {
+        return $data[0] * $data[1];
+    }
+
+    public static function result(array $data): int
+    {
+        $foundNumbers = static::findNumbersWhereSumEquals2020($data);
+
+        return static::multiplication($foundNumbers);
+    }
 }
--- tests/Day1/ReportRepairTest.php
+++ tests/Day1/ReportRepairTest.php
@@ -2,13 +2,38 @@
 
 namespace Opmvpc\Advent\Tests\Day1;
 
+use Opmvpc\Advent\Day1\ReportRepair;
 use PHPUnit\Framework\TestCase;
 
 class ReportRepairTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function additionResultIs2020()
     {
-        $this->assertTrue(true);
+        $result = ReportRepair::findNumbersWhereSumEquals2020($this->getTestData());
+        $this->assertEquals(2020, array_sum($result));
+    }
+
+    /** @test */
+    public function multiplicationIsOk()
+    {
+        $result = ReportRepair::multiplication([11, 53]);
+        $this->assertEquals(583, $result);
+    }
+
+    private function getTestData(): array
+    {
+        $data = file_get_contents(__DIR__.'/ReportRepairData.txt');
+        $explodedData = explode("\n", $data);
+        $filterdData = array_filter($explodedData, fn ($number) => $number !== '');
+
+        return $filterdData;
+    }
+
+    /** @test */
+    public function getResult()
+    {
+        $result = ReportRepair::result($this->getTestData());
+        $this->assertEquals(211899, $result);
     }
 }
