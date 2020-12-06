fd406e0dc76ed9f6781da8a2a628fcb48b83a4c0
opmvpc <thibsix@outlook.be>
2020-12-06 21:18:36 +0100

    oops oubli de comments

--- src/Day4/PassportProcessingParser.php
+++ src/Day4/PassportProcessingParser.php
@@ -26,20 +26,5 @@ class PassportProcessingParser extends AbstractParser
                 ;
             })
             ;
-
-        // $explodedData = explode("\n\n", $this->fileContent);
-        // $filteredData = array_filter($explodedData, fn (string $line) => $line !== '');
-        // $passportsData = str_replace("\n", ' ', $filteredData);
-        // $passportsData = array_map(function (string $passportData) {
-        //     $passportData = explode(' ',  $passportData);
-        //     $passportData = array_filter($passportData, fn (string $line) => $line !== '');
-        //     $passportData = array_map(fn (string $passport) => explode(':',  $passport), $passportData);
-        //     $passportData = array_map(fn (array $passport) => [$passport[0] => $passport[1]], $passportData);
-        //     $passportData = array_merge(...$passportData);
-
-        //     return $passportData;
-        // }, $passportsData);
-
-        // return $passportsData;
     }
 }
